98d3dfff6524a8cb2f9da249ba462911a8891553
vioif <ireneaflores3@gmail.com>
2018-01-04 17:48:29 -0500

    erb basics completed

--- dialogue.rb
+++ dialogue.rb
@@ -1,15 +1,17 @@
 require 'erb'
 
+price = 8.75
+
 line_1 = "Bill:   That will cost you <%= price %> plus taxes, ma'am."
 line_2 = "Pritma: How much is it with taxes?"
 line_3 = "Bill:   I don't know! I'm not very good at math."
 
 puts
-puts line_1
+puts ERB.new(line_1).result
 puts
 puts line_2
 puts
-puts line_3
+puts ERB.new(line_3).result
 puts
 
 def price
